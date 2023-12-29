5a2cfd8895052590c8bbfa25dab7bc6d9e227f2b
zeluiz78 <75767436+zeluiz78@users.noreply.github.com>
2023-12-29 14:33:09 -0300

    Add files via upload

---
+++ DESAFIO  Script de criação de tabelas e seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.tb_tipo_vinho(nome) VALUES ('Tinto');
+INSERT INTO public.tb_tipo_vinho(nome) VALUES ('Branco');
+INSERT INTO public.tb_tipo_vinho(nome) VALUES ('Rose');
+INSERT INTO public.tb_tipo_vinho(nome) VALUES ('Bordeaux');
